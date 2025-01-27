9a9a1b616213b5fe5be045b659963eaaead66bc5
root <root@PaulPC.>
2025-01-28 00:29:58 +0200

    Paul has used script to add names of all group members

--- the_coder.sh
+++ the_coder.sh
@@ -2,3 +2,16 @@
 mkdir students_directory
 cd students_directory
 touch cohort_1 cohort_2 cohort_3
+
+output_file="cohort_1"
+
+> "$output_file"
+
+names=("Peter Udochukwu Opara-(Group Leader)" "Nkem Vincent Nweke" "Paul Masamvu" "Delphin Ruyumbu Cyusa" "Kevin Nyawakira" "King Obafemi Abejirin")
+
+for name in "${names[@]}"
+do
+    echo "$name" >> "$output_file"
+done
+
+echo "Names have been written to $output_file"
